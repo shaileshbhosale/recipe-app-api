05fb88437d5592bbf7dd3e76c23d1e8f9a9ccd7a
Shailesh Bhosale <shailesh.bhosale@globant.com>
2026-04-01 10:20:07 +0530

    Fix flake8 linting errors and remove obsolete docker-compose version

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator fucntions
 """
 
+
 def add(x, y):
     """Add x and y and return result."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result."""
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class Commandtests(SimpleTestCase):
     """Test Commands."""
