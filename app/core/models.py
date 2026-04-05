4fa86d4ce324f20730b15eef82dc81fa516ca232
Shailesh Bhosale <shailesh.bhosale@globant.com>
2026-04-05 12:04:18 +0530

    Fix linting error.

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+
 class Usermanager(BaseUserManager):
     """Manager for User."""
 
@@ -29,6 +30,7 @@ class Usermanager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
